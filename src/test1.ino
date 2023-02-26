66486b3ef25bf37243748a0de702e8da416617a1
EpsilonZero885 <kylemrucker@gmail.com>
2023-02-26 14:37:28 -0500

    Changed pin variable types from int to uint8_t. Added descriptive comments. Initialized pins with pullups. Created a wait loop for serial monitor initialization

--- src/test1.ino
+++ src/test1.ino
@@ -8,97 +8,97 @@
  1 x Tap Switch Module
  1 x Tilt Switch Module
  1 x Shock Switch Module
- 
  */
 
 
-#include <Arduino.h>
+/* Include Libraries */
+#include <Arduino.h> //for VSCode
 
-//Pin connected to ST_CP of 74HC595
-int latchPin = 6;
+/* Global Variables */
+const uint8_t latchPin = 6; //Pin connected to ST_CP of 74HC595
+const uint8_t clockPin = 5; //Pin connected to SH_CP of 74HC595
+const uint8_t dataPin = 7; //Pin connected to DS of 74HC595
+const uint8_t tapSense = 9; //Data pin from Tap Switch
+const uint8_t shockSense = 10; //Data pin from Shock Switch
+const uint8_t tiltSense = 11; //Data pin from Tilt Switch
 
-//Pin connected to SH_CP of 74HC595
-int clockPin = 5;
-
-////Pin connected to DS of 74HC595
-int dataPin = 7;
-
-int tapSense = 9; //Data from Tap Switch
-int shockSense = 10; //Data from Shock Switch
-int tiltSense = 11; //Data from Tilt Switch
-
-
+/* Setup Loop */
 void  setup() {
+  /* Initialize Serial Monitor: */
+  Serial.begin(9600); //Start Serial Communication at 9600 baud rate
+  if (!Serial) {
+    while (1); //Wait for Serial Monitor to open
+  } //end if (!Serial)
 
-  Serial.begin(9600);
-
+  /* Initialize Pins */
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(tapSense, INPUT_PULLUP);
+  pinMode(shockSense, INPUT_PULLUP);
+  pinMode(tiltSense, INPUT_PULLUP);
+} //end setup()
 
-  pinMode(tapSense, INPUT);
-  pinMode(shockSense, INPUT);
-  pinMode(tiltSense, INPUT);
-
-}
-
-
+/* Main Loop*/
 void  loop() {
+  /* Read Sensors */
+  bool tapRead = digitalRead(tapSense); //Read the state of the Tap Sensor
+  bool shockRead = digitalRead(shockSense); //Read the state of the Shock Sensor
+  bool tiltRead = digitalRead(tiltSense); //Read the state of the Tilt Sensor
 
-  int tapRead = digitalRead(tapSense);
-  int shockRead = digitalRead(shockSense);
-  int tiltRead = digitalRead(tiltSense);
+  /* Clear Shift Register */
+  digitalWrite(latchPin, LOW); //Open the latch
+  shiftOut(dataPin, clockPin, MSBFIRST, 0); //Shift the bits, 0 = 000000
+  digitalWrite(latchPin, HIGH); //Close the latch
 
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  digitalWrite(latchPin, HIGH);
-
-
-   if (tapRead==LOW) { //Low when Triggered
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, 48);
-    digitalWrite(latchPin, HIGH);
-
+  /* Check Tap Sensor */
+  if (tapRead == LOW) { //Low when Triggered
+    digitalWrite(latchPin, LOW); //Open the latch
+    shiftOut(dataPin, clockPin, MSBFIRST, 48); //Shift the bits, 48 = 110000
+    digitalWrite(latchPin, HIGH); //Close the latch
+    
+    /* For Testing */
     Serial.print("tapSense = ");
     Serial.println (tapRead);
 
-   int tapRead = digitalRead(tapSense);
-   int shockRead = digitalRead(shockSense);
-   int tiltRead = digitalRead(tiltSense);
+    /* Read Pin States */
+    bool tapRead = digitalRead(tapSense); //Read the state of the Tap Sensor
+    bool shockRead = digitalRead(shockSense); //Read the state of the Shock Sensor
+    bool tiltRead = digitalRead(tiltSense); //Read the state of the Tilt Sensor
+    delay(1000); //Delay 1 second
+  } //end if (tapRead==LOW)
 
-           delay(1000);
-   }
-
-
-  if (shockRead==LOW) { //Low when Triggered
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, 12);
-    digitalWrite(latchPin, HIGH);
-
+  /* Check Shock Sensor */
+  if (shockRead == LOW) { //Low when Triggered
+    digitalWrite(latchPin, LOW); //Open the latch
+    shiftOut(dataPin, clockPin, MSBFIRST, 12); //Shift the bits, 12 = 001100
+    digitalWrite(latchPin, HIGH); //Close the latch
+    
+    /* For Testing */
     Serial.print("shockSense = ");
     Serial.println (shockRead);
 
-    
-   int tapRead = digitalRead(tapSense);
-   int shockRead = digitalRead(shockSense);
-   int tiltRead = digitalRead(tiltSense);
+    /* Read Pin States */
+    bool tapRead = digitalRead(tapSense); //Read the state of the Tap Sensor
+    bool shockRead = digitalRead(shockSense); //Read the state of the Shock Sensor
+    bool tiltRead = digitalRead(tiltSense); //Read the state of the Tilt Sensor
+    delay(1000); //Delay 1 second
+  } //end if (shockRead==LOW)
 
-          delay(1000);
-   }
+  /* Check Tilt Sensor */
+  if (tiltRead == LOW) { //Low when Triggered
+    digitalWrite(latchPin, LOW); //Open the latch
+    shiftOut(dataPin, clockPin, MSBFIRST, 3); //Shift the bits, 3 = 000011
+    digitalWrite(latchPin, HIGH); //Close the latch
 
-
-  if (tiltRead==LOW) { //Low when Triggered
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, 3);
-    digitalWrite(latchPin, HIGH);
-
+    /* For Testing */
     Serial.print("tiltSense = ");
     Serial.println (tiltRead);
-    
-   int tapRead = digitalRead(tapSense);
-   int shockRead = digitalRead(shockSense);
-   int tiltRead = digitalRead(tiltSense);
 
-          delay(1000);
-   }
-}
+    /*Read Pin States*/
+    bool tapRead = digitalRead(tapSense); //Read the state of the Tap Sensor
+    bool shockRead = digitalRead(shockSense); //Read the state of the Shock Sensor
+    bool tiltRead = digitalRead(tiltSense); //Read the state of the Tilt Sensor
+    delay(1000); //Delay 1 second
+  } //end if (tiltRead==LOW)
+} //end loop()
